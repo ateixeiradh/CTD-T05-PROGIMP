0e6b448ac6f883fe02a9a5ab7e4f6e4fe003e1e6
Ana <ateixeira@digitalhouse.com>
2021-12-06 21:33:54 -0300

    Co-authored-by: Matheus Ferreira <matheusrayol@users.noreply.github.com> Co-authored-by: gabrielcesar48@hotmail.com <gabrielcesar48@hotmail.com> Co-authored-by: matheus.almeida.sobrinho@hotmail.com <matheus.almeida.sobrinho@hotmail.com> Co-authored-by: schelen01 <schelen01@users.noreply.github.com> Co-authored-by: abdu02 <abdu02@users.noreply.github.com> Co-authored-by: bkast@hotmail.com <bkast@hotmail.com> Co-authored-by: Carlos Junior <CJBiohacker@users.noreply.github.com> Co-authored-by: Michaelle Lima <michaellelima@users.noreply.github.com> Co-authored-by: Tatiane Gardin  <tatianegardin@users.noreply.github.com> Co-authored-by: Lucas Ramalho <Lucas-Ramalho@users.noreply.github.com> Co-authored-by: Juan Evangelista Nascimento  <juanevangelista1@users.noreply.github.com> Co-authored-by: FERNANDA RIBEIRO DE MAGALHAES <nandamagalhaes26@gmail.com> Co-authored-by: WALLACE DE JESUS COSTA ASSIS <Wallaceadm@users.noreply.github.com> Co-authored-by: azcarpes@hotmail.com <azcarpes@hotmail.com>

---
+++ Revisão/revisao.js
@@ -0,0 +1,25 @@
+// 1 - PENSAMENTO COMPUTACIONAL
+// DEFINIÇÃO: 
+//é uma decomposição(fragmentação) de uma ideia, problema
+
+// 2 - AMBIENTE DE DESENVOLVIMENTO
+
+// 3 - FUNÇÕES
+
+// 4 - OPERADORES
+
+// 5 - CONTROLE DE FLUXO DA APLICAÇÃO
+
+// 6 - STRING E ARRAY
+
+// 7 - ESTRUTURA DE REPETIÇÃO
+
+// 8 - OBJETOS E JSON
+
+// 9 - MÓDULOS E PACOTES
+
+// 10 - FUNÇÕES AVANÇADAS
+
+
+
+
