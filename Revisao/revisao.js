749780982927f3556a73da806810c062640a6cb2
Ana <ateixeira@digitalhouse.com>
2021-12-06 22:56:54 -0300

    Co-authored-by: Matheus Ferreira <matheusrayol@users.noreply.github.com> Co-authored-by: gabrielcesar48@hotmail.com <gabrielcesar48@hotmail.com> Co-authored-by: matheus.almeida.sobrinho@hotmail.com <matheus.almeida.sobrinho@hotmail.com> Co-authored-by: schelen01 <schelen01@users.noreply.github.com> Co-authored-by: abdu02 <abdu02@users.noreply.github.com> Co-authored-by: bkast@hotmail.com <bkast@hotmail.com> Co-authored-by: Carlos Junior <CJBiohacker@users.noreply.github.com> Co-authored-by: Michaelle Lima <michaellelima@users.noreply.github.com> Co-authored-by: Tatiane Gardin  <tatianegardin@users.noreply.github.com> Co-authored-by: Lucas Ramalho <Lucas-Ramalho@users.noreply.github.com> Co-authored-by: Juan Evangelista Nascimento  <juanevangelista1@users.noreply.github.com> Co-authored-by: FERNANDA RIBEIRO DE MAGALHAES <nandamagalhaes26@gmail.com> Co-authored-by: WALLACE DE JESUS COSTA ASSIS <Wallaceadm@users.noreply.github.com> Co-authored-by: azcarpes@hotmail.com <azcarpes@hotmail.com> Co-authored-by: rodrigues-saraiva@hotmail.com <rodrigues-saraiva@hotmail.com> Co-authored-by: erickniciolli <erickniciolli@users.noreply.github.com> Co-authored-by: GiMGalvao <GiMGalvao@users.noreply.github.com> Co-authored-by: Jonata santos  <Jonatacosta@users.noreply.github.com>

--- Revisao/revisao.js
+++ Revisao/revisao.js
@@ -40,6 +40,8 @@
     // null, representa um valor nulo ou "vazio" ( ligada a variavel booleana)
     // eles são tipos de dados? NÃO!
 
+    // VERIFICAR O TIPO DE DADOS - método já definido do JS - 'type of'
+
     // ESTRUTURA DA FUNÇÃO
     // nome da função, palavra reservada 'function', return
     // parâmetro é opcional
@@ -48,31 +50,54 @@
     // function DH (x,y){ return x+y }
     
 // 4 - OPERADORES
-    // DE ATRIBUIÇÃO --> '=' , '+=', '*='
-    // COMPARATIVOS --> '==' , '===', '!=', '!==', '>', '<', '<=', '>=' 
-    // ARITMÉTICOS --> + , -, *, /, **
-    // LÓGICOS --> '&&', '||', '!x'
-    // DE STRING --> console.log("String X" + "String Y");
-    // CONDICIONAL(TERNÁRIO) --> (condição) ? resultado(true)1 : resultado 2 ; (passa a condição, se for verdadeira retorna a primeira, os 2 pontos são o "else/ senão" passa o o falso)
-    // UNÁRIO --> 'delete' , 'type of', 'void (expressão)';   
-    // RELACIONAL --> 'in' , 'instanceof'
+    // DE ATRIBUIÇÃO --> '=' , '+=', '*=' (antes de fazer atribuição ele vai fazer a operação)
+    // Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_Operators#operador_atribuicao
+
+    // [fruta1, fruta2, fruta3, fruta4]
+    // [a, b, c]
+    // É uma desestruturação porque na sintaxe do código em questão,
+    // quando a variável [fruta1, fruta2, fruta3] recebe um array com a mesma quantidade de posições,
+    // o JS entende que ouve aconteceu uma atribuição via desestruturação,
+    // que significa extrair dados de arrays ou objetos em variáveis distintas de acordo com a sua posição.
+    // Sendo, a primeira posição da variável [fruta1, fruta2, fruta3] recebeu a primeira posição do array frutasAmarelas.
+    // Referência: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+    // COMPARATIVOS --> '==' (valor) , '===' (tipo e valor - estritamente igual), '!=' (diferente), '!==' (estritamente não igual),
+    // '>' (maior que), '<' (menor que), '<=' (menor ou igual), '>=' (maior ou igual) --> COMPARAÇÕES SIMPLES
+
+    // '===' e '!==' --> COMPARAÇÃO ESTRITA sempre considerar valor e tipo
+
+    // ARITMÉTICOS/MATEMÁTICOS --> + , -, *, /, ** (expoente), % (resto da divisão)
+    // INCREMENTO/DECREMENTO --> ++ -- (i = i + 1 é o i++, ++i)
+    
+    // LÓGICOS --> '&&' (AND (E)), '||' (OR (OU)), '!x' (NOT (NEGAÇÃO))
+
+    // DE STRING --> console.log("String X" + "String Y"); --> concatenação
+
+    // VALIDAR TIPOS
+        // ao invés de usar o ESTRITO, pode usar 'type of'
 
 // 5 - CONTROLE DE FLUXO DA APLICAÇÃO
+    // IF-ELSE
+        // IF - ELSE IF - ELSE
+
+    // CONDICIONAL(TERNÁRIO) 
+        // --> (condição) ? resultado(true)1 : resultado 2 ;
+        // (passa a condição, se for verdadeira retorna a primeira, os 2 pontos são o "else/ senão" passa o o falso)
+
+    // SWITCH CASE
+
+    
 
 // 6 - STRING E ARRAY
  
 // 7 - ESTRUTURA DE REPETIÇÃO
+    // WHILE
+    // DO-WHILE
+   // FOR
                                              
 // 8 - OBJETOS E JSON
 
 // 9 - MÓDULOS E PACOTES
 
 // 10 - FUNÇÕES AVANÇADAS
-
-
-vim aqui pra baixo
-opa cheguei
-
-prox
-o.o
-
